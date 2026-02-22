0550bc423c9676e5bc7709f1da83cdc49169d52c
Muhtadana1 <162767147+Muhtadana1@users.noreply.github.com>
2026-02-22 10:00:37 +0700

    Add files via upload

---
+++ JS/JSFunction.js
@@ -0,0 +1,11 @@
+﻿function FormatUSNumeric(value) {
+    // PERBAIKAN: Ubah 'value' menjadi String dulu, (value || 0) untuk menangani null
+    const stringValue = String(value || 0); 
+
+    const number = parseFloat(stringValue.replace(/[^0-9.-]/g, ''));
+    if (isNaN(number)) return '0.00'; // Selalu kembalikan 2 desimal
+    return new Intl.NumberFormat('us-EN', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }).format(number);
+}
